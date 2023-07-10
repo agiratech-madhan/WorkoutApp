c2831b08d58c09a0a0d7aa2ebc02eec3bff358bc
Madhan-Rkv <madhanrkv10@gmail.com>
2023-07-10 12:28:37 +0530

    Backpressed Funtionality

--- app/src/main/java/com/example/workoutapp/ExcerSizeActivity.kt
+++ app/src/main/java/com/example/workoutapp/ExcerSizeActivity.kt
@@ -14,6 +14,7 @@ class ExcerSizeActivity : AppCompatActivity() {
         if(supportActionBar!=null){
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
-        binding?.toolbarExcerSize?.setNavigationOnClickListener {  }
+        binding?.toolbarExcerSize?.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()        }
     }
 }
