f3e91bceb051166350534a69b3b9e71713927d4e
Madhan-Rkv <madhanrkv10@gmail.com>
2023-07-10 12:08:36 +0530

    added appbar and Navigation Function

---
+++ app/src/main/java/com/example/workoutapp/ExcerSizeActivity.kt
@@ -0,0 +1,19 @@
+package com.example.workoutapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.workoutapp.databinding.ActivityExcerSizeBinding
+
+class ExcerSizeActivity : AppCompatActivity() {
+    private  var binding: ActivityExcerSizeBinding?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityExcerSizeBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setSupportActionBar(binding?.toolbarExcerSize)
+        if(supportActionBar!=null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        binding?.toolbarExcerSize?.setNavigationOnClickListener {  }
+    }
+}
--- app/src/main/java/com/example/workoutapp/MainActivity.kt
+++ app/src/main/java/com/example/workoutapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.workoutapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
@@ -14,11 +15,8 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding?.root)
         binding?.flStart?.setOnClickListener {
-            Toast.makeText(
-                this@MainActivity,
-                "Here we will start the exercise.",
-                Toast.LENGTH_SHORT
-            ).show()
+          val intent= Intent(/* packageContext = */ this, /* cls = */ ExcerSizeActivity::class.java)
+            startActivity(intent)
         }
     }
 }
