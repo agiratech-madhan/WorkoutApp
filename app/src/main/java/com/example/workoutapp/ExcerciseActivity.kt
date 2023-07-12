34e7158b666489430201fb47951cc74dd883013a
Madhan-Rkv <madhanrkv10@gmail.com>
2023-07-12 15:32:50 +0530

    timer based image changes with imageModel

---
+++ app/src/main/java/com/example/workoutapp/ExcerciseActivity.kt
@@ -0,0 +1,133 @@
+package com.example.workoutapp
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+//import com.example.workoutapp.databinding.ActivityExcerSizeBinding
+import com.example.workoutapp.databinding.ActivityExersizeBinding
+
+class ExcerSizeActivity : AppCompatActivity() {
+    private var restTimer: CountDownTimer? =
+        null // Variable for Rest Timer and later on we will initialize it.
+    private var restProgress = 0
+    private var binding: ActivityExersizeBinding? = null
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 0
+    private var exerciseList: ArrayList<ExerciseModel>? = null
+    private var currentExersizePosition = -1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExersizeBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setSupportActionBar(binding?.toolbarExercise)
+        if (supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        exerciseList = Constants.defaultExerciseList()
+
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+        setupRestView()
+    }
+
+    private fun setupRestView() {
+
+        /**
+         * Here firstly we will check if the timer is running the and it is not null then cancel the running timer and start the new one.
+         * And set the progress to initial which is 0.
+         */
+        if (restTimer != null) {
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+
+        // This function is used to set the progress details.
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar() {
+
+        binding?.progressBar?.progress =
+            restProgress // Sets the current progress to the specified value.
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++ // It is increased by 1
+                binding?.progressBar?.progress =
+                    10 - restProgress // Indicates progress bar progress
+                binding?.tvTimer?.text =
+                    (10 - restProgress).toString()  // Current progress is set to text view in terms of seconds.
+            }
+
+            override fun onFinish() {
+                // When the 10 seconds will complete this will be executed.
+//                Toast.makeText(
+//                    this@ExcerSizeActivity, "The Message timer.", Toast.LENGTH_SHORT
+//                ).show()
+                currentExersizePosition++
+                setupExerciseView()
+            }
+        }.start()
+    }
+
+    private fun setupExerciseView() {
+
+        // Here according to the view make it visible as this is Exercise View so exercise view is visible and rest view is not.
+        binding?.flRestView?.visibility = View.INVISIBLE
+        binding?.tvTitle?.visibility = View.INVISIBLE
+        binding?.tvExerciseName?.visibility = View.VISIBLE
+        binding?.flExerciseView?.visibility = View.VISIBLE
+        binding?.ivImage?.visibility = View.VISIBLE
+
+        /**
+         * Here firstly we will check if the timer is running and it is not null then cancel the running timer and start the new one.
+         * And set the progress to the initial value which is 0.
+         */
+        if (exerciseTimer != null) {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+        binding?.ivImage?.setImageResource(exerciseList!![currentExersizePosition].getImage())
+
+        binding?.tvExerciseName?.text=exerciseList!![currentExersizePosition].getName()
+
+        setExerciseProgressBar()
+    }
+
+    private fun setExerciseProgressBar() {
+
+        binding?.progressBarExercise?.progress = exerciseProgress
+
+        exerciseTimer = object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                binding?.progressBarExercise?.progress = 30 - exerciseProgress
+                binding?.tvTimerExercise?.text = (30 - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                if(currentExersizePosition<exerciseList?.size!!-1){
+                    setupRestView()
+                }else{
+                    Toast.makeText(
+                    this@ExcerSizeActivity,
+                    "Congratulations! You have Completed 7 minutes Workout",
+                    Toast.LENGTH_SHORT
+                ).show()
+                }
+//
+            }
+        }.start()
+    }
+
+    public override fun onDestroy() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        super.onDestroy()
+        binding = null
+    }
+}
