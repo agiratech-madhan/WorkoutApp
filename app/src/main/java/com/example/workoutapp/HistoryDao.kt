e49ec8df8795036de0ebbf1dc512335dc1714101
Madhan-Rkv <madhan.k@agiratech.com>
2023-07-19 12:07:40 +0530

    Displaying the Workout History on our Recyclerview

--- app/src/main/java/com/example/workoutapp/HistoryDao.kt
+++ app/src/main/java/com/example/workoutapp/HistoryDao.kt
@@ -2,10 +2,14 @@ package com.example.workoutapp
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface HistoryDao {
 
     @Insert
     suspend fun insert(historyEntity: HistoryEntity)
+    @Query("Select * from `history-table`")
+    fun fetchALlDates(): Flow<List<HistoryEntity>>
 }
